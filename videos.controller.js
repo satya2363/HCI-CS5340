928d3f670f87f563025f6c053a4b2b95aa2aea9e
hardik-surana <surana.h@husky.neu.edu>
2016-12-05 21:31:43 -0500

    Added intial videos page

---
+++ videos.controller.js
@@ -0,0 +1,23 @@
+/**
+ * Created by hardik on 1/12/2016.
+ */
+
+
+
+(function(){
+    angular
+        .module("BHC")
+        .controller("VideosController",VideosController);
+
+
+    function TeamController() {
+        var vm = this;
+
+        function init() {}
+
+        init();
+
+    }
+
+})();
+
